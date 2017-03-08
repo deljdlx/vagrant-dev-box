07cfffc785474fc58958d5da7e6efc335a5d4c6f
ElBIniou <jdlxtrash@gmail.com>
2017-03-08 09:34:45 +0100

    synchro

--- provision.sh
+++ provision.sh
@@ -1,6 +1,25 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install -y vim
-sudo apt-get install -y git
+sudo -s
+apt-get update && apt-get upgrade && apt-get dist-upgrade -y
+apt-get update
+apt-get upgrade
+apt-get install -y vim
+apt-get install -y git
+apt-get install -y pkg-config
 
 
+echo 'deb http://packages.dotdeb.org jessie all' >> /etc/apt/sources.list
+echo 'deb-src http://packages.dotdeb.org jessie all' >> /etc/apt/sources.list
+wget https://www.dotdeb.org/dotdeb.gpg && apt-key add dotdeb.gpg
+apt-get update
+apt-get install -y php7.0
+apt-get install -y php7.0-dev
+apt-get install -y libzmq3-dev
+apt-get install -y php-pear
+pecl channel-update pecl.php.net
+
+
+
+pecl install "channel://pecl.php.net/zmq-1.1.3"
+echo 'extension=zmq.so' > /etc/php/7.0/mods-available/zmq.ini
+echo 'extension=zmq.so' > /etc/php/7.0/cli/conf.d/20-zmq.ini
+ln -s /etc/php/7.0/mods-available/zmq.ini /etc/php/7.0/apache2/conf.d/zmq.ini
