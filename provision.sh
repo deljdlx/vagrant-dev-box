f52dac81bf702a962eaf95a801f0d388c303750d
Julien Delsescaux <julien.delsescaux@habitatpresto.com>
2017-06-12 13:30:06 +0200

    synchro

--- provision.sh
+++ provision.sh
@@ -1,3 +1,9 @@
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
 sudo -s
 apt-get update && apt-get upgrade && apt-get dist-upgrade -y
 
